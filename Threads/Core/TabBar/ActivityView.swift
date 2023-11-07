9e5a2c318f2a9351dfdffdd9fe4a8fd9aae94ffd
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-11-07 04:30:33 -0300

    Agora fiz a minha tabBAR com exploreView,createView, profileView, ActivityView, coloquei tag, um foreground black

---
+++ Threads/Core/TabBar/ActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityView.swift
+//  Threads
+//
+//  Created by leonardo Moreira on 07/11/23.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityView()
+}
---
+++ Threads/Core/TabBar/CreateThreadView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateThreadView.swift
+//  Threads
+//
+//  Created by leonardo Moreira on 07/11/23.
+//
+
+import SwiftUI
+
+struct CreateThreadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateThreadView()
+}
