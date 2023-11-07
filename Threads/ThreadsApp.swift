2a7fdb76a83db88af7628eb48f199709d0459f2f
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-11-07 02:52:42 -0300

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by leonardo Moreira on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
