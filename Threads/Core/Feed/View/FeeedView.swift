762cdfb02886865fa23ee99c98418ac65b786fae
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-11-08 03:30:26 -0300

    fiz a arquitetura do feed !

---
+++ Threads/Core/Feed/View/FeeedView.swift
@@ -0,0 +1,23 @@
+//
+//  FeedView.swift
+//  Threads
+//
+//  Created by leonardo Moreira on 08/11/23.
+//
+
+import SwiftUI
+
+struct FeeedView: View {
+    var body: some View {
+        
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    FeeedView()
+}
---
+++ Threads/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadCell.swift
+//  Threads
+//
+//  Created by leonardo Moreira on 08/11/23.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
