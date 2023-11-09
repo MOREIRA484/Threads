0c36c379403b6ce62068f3d375ca0ccc1e85f3f4
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-11-09 02:17:20 -0300

    FIZ A ARQUITETURA PARA EDITAR O MEU PERFIL COMO BIO, NOME, FOTO !

---
+++ Threads/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  Threads
+//
+//  Created by leonardo Moreira on 09/11/23.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
