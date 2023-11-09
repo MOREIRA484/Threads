6de70f13d9c8e2fa3d10a5f331131ab32ea3a303
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-11-09 03:56:47 -0300

    fiz o viewModel da parte do registro para conseguir implementar no firebase. para o usuário criar a sua conta !

---
+++ Threads/Core/Authentication/Service/AuthService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthService.swift
+//  Threads
+//
+//  Created by leonardo Moreira on 09/11/23.
+//
+
+import Foundation
---
+++ Threads/Core/Authentication/ViewModel/RegistrationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationViewModel.swift
+//  Threads
+//
+//  Created by leonardo Moreira on 09/11/23.
+//
+
+import Foundation
