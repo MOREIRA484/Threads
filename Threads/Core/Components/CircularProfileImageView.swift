dec5b18c52672b7d43720478ad9ec3d8d63ea2ff
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-11-09 01:45:06 -0300

    fiz uma parte  do meu profile

---
+++ Threads/Core/Components/CircularProfileImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularProfileImageView.swift
+//  Threads
+//
+//  Created by leonardo Moreira on 08/11/23.
+//
+
+import SwiftUI
+
+struct CircularProfileImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularProfileImageView()
+}
---
+++ Threads/Core/Profile/Model/ProfileThreadFilter.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileThreadFilter.swift
+//  Threads
+//
+//  Created by leonardo Moreira on 08/11/23.
+//
+
+import Foundation
