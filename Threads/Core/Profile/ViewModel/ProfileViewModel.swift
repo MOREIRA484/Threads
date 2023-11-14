47032fd6d41ed1e3b820321e00eb63426db872f6
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-11-14 09:22:29 -0300

    codigo atualizado!

---
+++ Threads/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewModel.swift
+//  Threads
+//
+//  Created by leonardo Moreira on 11/11/23.
+//
+
+import Foundation
---
+++ Threads/Services/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  Threads
+//
+//  Created by leonardo Moreira on 11/11/23.
+//
+
+import Foundation
